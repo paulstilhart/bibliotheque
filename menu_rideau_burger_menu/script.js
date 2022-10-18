9877375397daa94d2a180c0d72a88d0d66c5273d
paulstilhart <paul.stilhart@gmail.com>
2022-10-18 16:11:38 +0100

    menu rideau

--- menu_rideau_burger_menu/script.js
+++ menu_rideau_burger_menu/script.js
@@ -1,8 +1,12 @@
-const burgerButton = document.querySelector(".nav_toggler");
-const navigation = document.querySelector(".nav");
+const burgerButton = document.querySelector(".nav_toggler");//On vient récupérer le bouton du burger menu
+const navigation = document.querySelector("nav");//egalement l'élément nav
 
+//Au click sur le bouton, on vient déclencher la fonction callback toggleNav
 burgerButton.addEventListener("click",toggleNav);
 
+//on définit la fonction toggleNav
+//toggle permet d'ajouter la classe si pas présente, l'enlever si déjà présente
 function toggleNav(){
-    burgerButton.classList.toggle("active")
+    burgerButton.classList.toggle("active");
+    navigation.classList.toggle("active");
 };
